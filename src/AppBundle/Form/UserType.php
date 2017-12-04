57b97fec445554d4a20efc4aadd7c52c6433ad81
Mihail Krasilnikov <mihail.krasilnikov.j@gmail.com>
2017-12-04 18:15:11 +0200

    Replaced DateType to Birthday type

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -3,14 +3,13 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 
 class UserType extends AbstractType
 {
@@ -20,7 +19,7 @@ class UserType extends AbstractType
             ->add('email', EmailType::class, ['attr' => ['class' => 'form-control']])
             ->add('firstName', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('lastName', TextType::class, ['attr' => ['class' => 'form-control']])
-            ->add('birthday', DateType::class, ['widget' => 'choice'])
+            ->add('birthday', BirthdayType::class, ['widget' => 'choice'])
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
                 'first_options'  => array(
