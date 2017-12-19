002317e3321a839a0c0bda0a805ebc228b65f6d8
Mihail Krasilnikov <mihail.krasilnikov.j@gmail.com>
2017-12-19 14:41:29 +0200

    Added class for creating registration form

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanja
+ * Date: 12.12.2017
+ * Time: 12:53
+ */
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, ['attr' => ['label' => 'form.firstname']])
+            ->add('lastName')
+            ->add('birthday', BirthdayType::class, ['widget' => 'choice']);
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+
+}
