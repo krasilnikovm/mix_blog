53bf8aa7459faea3ed51d6a1f2e1b3aef8db01e8
Mihail Krasilnikov <mihail.krasilnikov.j@gmail.com>
2017-12-25 20:31:43 +0200

    Removed methods for work with post

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,49 +15,4 @@ class DefaultController extends Controller
         return $this->render(':default:index.html.twig');
     }
 
-    /**
-     * @Route("/posts/{page}", name="posts", defaults={ "page": 1})
-     */
-    public function postsAction($page)
-    {
-        $countRecords = 5;
-        $offset = 1 * ($page - 1) * $countRecords;
-        $posts = $this->getDoctrine()
-            ->getRepository('AppBundle:Post')
-            ->findBy([],[], $countRecords, $offset );
-
-        $paginator = $this->get('knp_paginator'); //(1)
-        $target = $posts; //(2) array('a', 'b', ... 'u');
-        // uses event subscribers to paginate $target
-        $slice = $paginator->paginate($target, $page/*page*/, $countRecords/*limit*/); //(3)
-        // $slice is a pagination view, represents the paginated data
-        if(!$posts) {
-            throw $this->createNotFoundException("Not found page with number " . $page );
-        }
-
-
-        return $this->render(':default:posts.html.twig', ['posts' => $posts, 'pagination' => $slice]);
-    }
-
-    /**
-     * @Route("/post/{id}", name="post")
-     */
-    public function postAction($id)
-    {
-        $post = $this->getDoctrine()
-            ->getRepository('AppBundle:Post')
-            ->find($id);
-
-
-        if(!$post) {
-            throw $this->createNotFoundException("No post with id " . $id);
-        }
-
-        return $this->render(
-            ":default:post.html.twig", [
-                'article' => $post->getArticle(),
-                'title' => $post->getTitle(),
-                'author' => $post->getUser()->getUserName()]);
-    }
-
 }
